5fb49cf72563f2ebddad4375841f790bc6b59af9
mariocorral <mariotwitter2012@gmail.com>
2023-11-04 19:59:26 +0100

    GAME-INFO PAGE UPDATED

--- src/app/game-info/game-info.component.ts
+++ src/app/game-info/game-info.component.ts
@@ -9,7 +9,6 @@ import { Comment } from '../../models/comment.model';
 import { GameService } from '../../services/game.service';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-game-info',
   templateUrl: './game-info.component.html',
@@ -19,9 +18,12 @@ import { Router } from '@angular/router';
 export class GameInfoComponent implements OnInit {
   game: Result | null = null;
   comments: Comment[] = [];
+  displayedComments: Comment[] = [];
   newComment: string = '';
   user: User | null = null;
   screenshots?: Screenshot[];
+  public commentPage: number = 1;
+  public commentsPerPage: number = 3;
 
   constructor(
     private route: ActivatedRoute,
@@ -44,13 +46,13 @@ export class GameInfoComponent implements OnInit {
             this.game.screenshots = screenshots;
           }
         });
-  
+
         this.commentsService.getComments(+gameId).subscribe(comments => {
           this.comments = comments;
         });
       });
     }
-    
+
     if (this.authService.getToken()) {
       console.log('Token: ' + this.authService.getToken());
       this.authService.getCurrentUser().subscribe(
@@ -62,8 +64,84 @@ export class GameInfoComponent implements OnInit {
         }
       );
     }
+    this.loadComments();
   }
   
+  openModal(index: number): void {
+    const modalId = `screenshotModal-${index}`;
+    const modalElement = document.getElementById(modalId);
+    if (modalElement) {
+      modalElement.style.display = 'block';
+    }
+  }
+
+  closeModal(index: number): void {
+    const modalId = `screenshotModal-${index}`;
+    const modalElement = document.getElementById(modalId);
+    if (modalElement) {
+      modalElement.style.display = 'none';
+    }
+  }
+
+  loadComments(): void {
+    const gameId = this.route.snapshot.paramMap.get('id');
+    if (gameId) {
+      this.commentsService.getComments(+gameId).subscribe(comments => {
+        this.comments = comments.sort((a, b) => {
+          const dateA = a.date ? new Date(a.date).getTime() : 0; // Fallback to 0 if 'a.date' is undefined
+          const dateB = b.date ? new Date(b.date).getTime() : 0; // Fallback to 0 if 'b.date' is undefined
+          return dateB - dateA; // Sort in descending order
+        });
+        this.displayComments();  // This will take care of displaying only the first 3 comments
+      });
+    }
+  }
+
+  displayComments(): void {
+    const start = (this.commentPage - 1) * this.commentsPerPage;
+    let end = start + this.commentsPerPage;
+    const maxPages = Math.ceil(this.comments.length / this.commentsPerPage);
+
+    // Solo toma los comentarios que corresponden a la página actual.
+    this.displayedComments = this.comments.slice(start, end);
+
+    // Si estamos en la primera página o la última página y hay menos comentarios de los esperados,
+    // entonces llenamos con placeholders.
+    if (this.commentPage === 1 || this.commentPage === maxPages) {
+      const placeholdersCount = this.commentsPerPage - this.displayedComments.length;
+
+      // Agrega placeholders solo si es necesario.
+      for (let i = 0; i < placeholdersCount; i++) {
+        this.displayedComments.push(this.createPlaceholderComment());
+      }
+    }
+  }
+
+
+  // Método para crear un comentario placeholder.
+  // Método para crear un comentario placeholder.
+  createPlaceholderComment(): Comment {
+    return {
+      email: '',
+      userName: '',
+      gameId: this.game ? this.game.id : 0, // Suponiendo que un gameId de 0 es inválido.
+      content: '',
+      date: new Date, // Agregar fecha como null o dejar sin inicializar si es opcional
+    };
+  }
+
+
+
+  loadMoreComments(): void {
+    const maxPages = Math.ceil(this.comments.length / this.commentsPerPage);
+    this.commentPage++;
+    if (this.commentPage > maxPages) {
+      this.commentPage = 1;
+    }
+
+    this.displayComments();
+  }
+
   postComment(): void {
     if (this.user && this.newComment.trim() && this.game) {
       const comment: Comment = {
